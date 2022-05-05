2ec4e2326df2af925ce27c5b7edac7abb2f5ae3f
pooh <poohvp1147@gmail.com>
2022-05-05 21:58:34 +0700

    first commit

---
+++ scan.js
@@ -0,0 +1,39 @@
+const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  document.getElementById("btn").innerHTML = '<img src = "clipboard-check-fill.svg" width = "20" height = "20">';
+}
+
+
+
+// File based scanning
+const fileinput = document.getElementById('qr-input-file');
+fileinput.addEventListener('change', e => {
+  if (e.target.files.length == 0) {
+    // No file selected, ignore 
+    return;
+  }
+
+  // Use the first item in the list
+  const imageFile = e.target.files[0];
+  html5QrCode.scanFile(imageFile, /* showImage= */false)
+  .then(qrCodeMessage => {
+    // success, use qrCodeMessage
+    document.getElementById('jsml').innerHTML = qrCodeMessage
+    Swal.fire(
+      'Scan success',
+      '<span class="mesbox">'+ qrCodeMessage+ '</span>' + '<button onclick'+ ' = "copyToClipboard' + "('#jsml'" + ')" class = "BTN" id = "btn"><img src = "clipboard.svg" width = "20" height = "20"></button>',
+      'success'
+    )
+   // alert(qrCodeMessage)
+  })
+  .catch(err => {
+    // failure, handle it.
+    console.log(`Error scanning file. Reason: ${err}`)
+  });
+});
+html5QrCode.clear();
